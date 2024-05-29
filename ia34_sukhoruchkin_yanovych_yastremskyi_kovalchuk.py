dfeead19449f3e574fbbb7ccd2bd4bb7f6d79300
GlebTiK <55922929+GlebTiK@users.noreply.github.com>
2024-05-29 19:40:16 +0300

    Update code

--- ia34_sukhoruchkin_yanovych_yastremskyi_kovalchuk.py
+++ ia34_sukhoruchkin_yanovych_yastremskyi_kovalchuk.py
@@ -2,13 +2,58 @@ class TreeNode:
     def __init__(self, key):
         self.left = None
         self.right = None
-        self.val = key
+        self.key = key
+
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        if self.right is None and self.left is None:
+            line = '%s' % self.key
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.key
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
 
 def insert(root, key):
     if root is None:
         return TreeNode(key)
     else:
-        if root.val < key:
+        if root.key < key:
             root.right = insert(root.right, key)
         else:
             root.left = insert(root.left, key)
@@ -18,28 +63,28 @@ def find_and_count_occurrences(root, key):
     count = 0
     current = root
     while current:
-        if key < current.val:
+        if current.key == key:
+            count += 1
+        if key <= current.key:
             current = current.left
-        elif key > current.val:
+        elif key > current.key:
             current = current.right
         else:
-            if current.left and current.left.val == key:
-                count += 1
-                current = current.left
-            elif current.right and current.right.val == key:
-                count += 1
-                current = current.right
-            count += 1
             current = None
     return count
 
 root = TreeNode('m')
+insert(root, 'g')
 insert(root, 'g')
 insert(root, 't')
 insert(root, 'a')
 insert(root, 'k')
 insert(root, 'o')
 insert(root, 'z')
+insert(root, 'g')
+insert(root, 'z')
+
+root.display()
 
 key = input("Enter the character to find in the tree: ")
 count = find_and_count_occurrences(root, key)
